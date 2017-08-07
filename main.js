523a5b9bdbf6ced7a529578da3e618809a82a31e
Jared Mellentine <jared@mellentine.com>
2017-08-07 13:42:15 -0700

    Cleaned up the menu and fixed punctuation

--- main.js
+++ main.js
@@ -114,17 +114,10 @@ app.on('ready', function(){
   let template = [{
     label: "Application",
     submenu: [
-      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-    ]}, {
-    label: "Edit",
-    submenu: [
-      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-      { type: "separator" },
-      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
       { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
       { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      { type: 'separator' },
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
     ]}
   ];
   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
