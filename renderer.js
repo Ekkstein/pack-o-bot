055b8c2c365af486f0847310d793581632a92e28
Will Fentress <wfentress@users.noreply.github.com>
2017-08-12 08:00:45 +0000

    Add UI for custom directories

--- renderer.js
+++ renderer.js
@@ -6,6 +6,20 @@ electron.ipcRenderer.send('put-in-tray');
 let token_input = document.getElementById('token');
 token_input.value = win.token;
 
+document.getElementById('hearthstoneDir').setAttribute(
+  'placeholder',
+  process.platform === 'win32'
+  ? 'C:\\Program Files (x86)\\Hearthstone\\'
+  : '/Applications/Hearthstone/'
+);
+
+document.getElementById('dataDir').setAttribute(
+  'placeholder',
+  process.platform === 'win32'
+    ? require('os').homedir() + '\\AppData\\Local\\Blizzard\\Hearthstone\\'
+    : require('os').homedir() + '/Library/Preferences/Blizzard/Hearthstone/'
+);
+
 document.getElementById('submit').onclick = function(){
   electron.ipcRenderer.send('settings-changed', token_input.value);
 };
@@ -14,6 +28,19 @@ document.getElementById('token_link').onclick = function(e){
   e.preventDefault();
   electron.shell.openExternal(this.href);
 };
+
+document.body.querySelectorAll('.accordion > :first-child').forEach(element => {
+  element.onclick = function(e) {
+    e.preventDefault();
+    let accordion = e.target;
+    while (accordion !== null && !accordion.classList.contains('accordion')) {
+      accordion = accordion.parentNode;
+    }
+    if (accordion !== null) {
+      accordion.classList.toggle('collapsed');
+    }
+  };
+});
 
 electron.ipcRenderer.on('status-change', function (event, message) {
   document.getElementById('status').innerText = message;
