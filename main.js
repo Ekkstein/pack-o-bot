01b7c02a59ca32d52ebe5475b0ab650b9f73a051
Gerald Hiller <geraldhiller@gmail.com>
2020-03-29 10:26:27 +0200

    added 0.3.1 version number

--- main.js
+++ main.js
@@ -112,7 +112,7 @@ ipc.on('put-in-tray', function (event) {
 let setupContextMenu = function(){
   let contextMenu = Menu.buildFromTemplate([
     {
-      label: 'pack-o-bot v0.3.0',
+      label: 'pack-o-bot v0.3.1',
       enabled: false
     },
     {
